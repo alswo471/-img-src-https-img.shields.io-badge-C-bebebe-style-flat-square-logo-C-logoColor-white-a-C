a375907c7e7e27913033f83920d626e4bd32ba86
alswo471 <alswo471@naver.com>
2022-07-06 18:34:54 +0900

    공부 한 것

--- study02/Git_Java28.java
+++ study02/Git_Java28.java
@@ -1,5 +1,17 @@
 package study02;
 
 public class Git_Java28 {
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
 
+		int sum = add(num1, num2); // add() 함수 호출
+		System.out.println(num1 + " + " + num2 + " = " + sum + "입니다.");
+	}
+
+	// add() 함수
+	public static int add(int n1, int n2) {
+		int result = n1 + n2;
+		return result; // 결과 값 반환
+	}
 }
