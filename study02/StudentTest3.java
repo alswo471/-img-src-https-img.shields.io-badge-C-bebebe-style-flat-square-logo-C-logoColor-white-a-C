b252632a210e58a395d5fd4614b1318821eafb1e
alswo471 <alswo471@naver.com>
2022-07-17 23:39:55 +0900

    공부

--- study02/StudentTest3.java
+++ study02/StudentTest3.java
@@ -2,17 +2,14 @@ package study02;
 
 public class StudentTest3 {
 	public static void main(String[] args) {
-		Student studentLee = new Student();
-		//studentLee.studentName = "이상원"; // 오류발생
+		Student3 studentLee = new Student3();
+		studentLee.setStudentName("이지원");
+		System.out.println(studentLee.serialNum); // serialNum 변수의 초깃값 출력
+		studentLee.serialNum++; // static 변수 값 증가
 		
-		// setStudentName() 메서드 활용해 private 변수에 접근 가능
-		studentLee.setStudentName("이상원");
-		
-		System.out.println(studentLee.getStudentName());
-		
-		/**
-		 * studentName 멤버 변수에 이름 값을 직접 대입하는 것이 아니고 setStudentName()메서드를 활용하여 값을 대입할 수 있습니다.
-		 * 즉 외부 클래스에서 private 변수에 직접 접근할 수는 없지만, public 메서드를 통하면 private변수에 접근할 수 있습니다.
-		 */
+		Student3 studentSon = new Student3();
+		studentSon.setStudentName("손수경");
+		System.out.println(studentSon.serialNum);  //증가된 값 출력
+		System.out.println(studentLee.serialNum); // 증가된 값 출력
 	}
 }
